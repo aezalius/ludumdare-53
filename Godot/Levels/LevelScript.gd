85781f8065b5efad6d1343add5adeccd46e0dfba
aezalius <thebaconcraft@gmail.com>
2023-04-29 12:34:32 -0400

    Added a basic encounter

--- Godot/Levels/LevelScript.gd
+++ Godot/Levels/LevelScript.gd
@@ -1,14 +1,24 @@
 extends Node2D
 
 @export_subgroup("Important locations")
-@export var package_spawn_locations: Array[Node2D]
 @export var encounter_table: Array[PackedScene]
+
+var active_encounter: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	QuestHandler.spawn_encounter.connect(spawn_encounter)
+	QuestHandler.remove_encounter.connect(remove_encounter)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func spawn_encounter():
+	var encounter: Node2D = encounter_table[randi_range(0,encounter_table.size() - 1)].instantiate()
+	#encounter.global_position = encounter.get_spawn_location()#encounter_spawn_locations[randi_range(0,encounter_spawn_locations.size() - 1)] # godot wont let me do things nicely, so hacky it is!
+	get_tree().root.add_child(encounter)
+	active_encounter = encounter
+
+func remove_encounter():
+	active_encounter.queue_free()
--- Godot/Scripts/Global/QuestHandler.gd
+++ Godot/Scripts/Global/QuestHandler.gd
@@ -2,6 +2,8 @@ extends Node
 
 signal next_dialogue_line_s(line: String)
 signal clear_dialogue()
+signal spawn_encounter()
+signal remove_encounter()
 
 # Effectively stops player input, with things for future.
 signal stop_movement()
@@ -14,25 +16,35 @@ var thanks = ["Thank you"]
 
 var current_speech: Array
 var speech_index = 0
+var spawn_encounter_after_dialogue = false
+var free_encounter_with_dialogue = false
 
 func generate_request_speech() -> void:
 	var selected_greeting = greeting[randi_range(0,greeting.size() - 1)]
 	var selected_request = request[randi_range(0,request.size() - 1)]
 	var selected_ending = ending[randi_range(0,ending.size() - 1)]
 	current_speech = [selected_greeting, selected_request, selected_ending]
+	spawn_encounter_after_dialogue = true
 
 func generate_thanks_speech() -> void:
 	var selected_greeting = greeting[randi_range(0,greeting.size() - 1)]
 	var selected_thanks = thanks[randi_range(0,thanks.size() - 1)]
 	current_speech = [selected_greeting, selected_thanks]
+	free_encounter_with_dialogue = true
 
 func next_dialogue_line():
 	if current_speech.size() > 0 and speech_index < current_speech.size():
 		emit_signal("stop_movement")
 		emit_signal("next_dialogue_line_s", current_speech[speech_index])
 		speech_index += 1
+		if free_encounter_with_dialogue:
+			emit_signal("remove_encounter")
+			free_encounter_with_dialogue = false
 	else:
 		emit_signal("free_movement")
 		emit_signal("clear_dialogue")
 		current_speech = []
 		speech_index = 0
+		if spawn_encounter_after_dialogue:
+			emit_signal("spawn_encounter")
+			spawn_encounter_after_dialogue = false
