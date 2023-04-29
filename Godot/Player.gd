aed9ffa1204643bc8039a0ce90c6231c36b8bfd8
aezalius <thebaconcraft@gmail.com>
2023-04-29 00:04:12 -0400

    added package, pickup and dropping

--- Godot/Player.gd
+++ Godot/Player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 @onready var sprite = $Sprite2D
 @onready var anim_player = $AnimationPlayer
 
+@onready var interact_area = $InteractArea
+
+@onready var package_sprite = $PackageSprite
+@onready var package_scene = preload("res://droppedpackage.tscn")
+
 const SPEED = 150.0
 var movement_enabled = true
 
@@ -11,7 +16,19 @@ var holding_package = false
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("menu"):
 		get_tree().quit()
-
+	if Input.is_action_just_pressed("roll"):
+		if not package_sprite.visible:
+			for body in interact_area.get_overlapping_bodies():
+				if body.is_in_group("package"):
+					package_sprite.show()
+					body.queue_free()
+		else:
+			package_sprite.hide()
+			var package_instance: RigidBody2D = package_scene.instantiate()
+			package_instance.global_position = package_sprite.global_position
+			get_tree().root.add_child(package_instance)
+			
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("left","right","up", "down");
@@ -36,3 +53,4 @@ func damage():
 		print("Player has died!")
 	else:
 		pass # drop package
+
